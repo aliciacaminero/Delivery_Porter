7bffc8b809a35d2726924b93e303346238ecaf73
Erove13 <ema.rosas.v@gmail.com>
2025-01-25 11:27:59 +0100

    Arreglo modelo

--- prueba_stream.py
+++ prueba_stream.py
@@ -1,13 +1,24 @@
 import streamlit as st
 import pandas as pd
 import joblib as joblib
-import os 
+import os
+import sklearn
 
 print(os.getcwd())
 
-modelo = joblib.load('/03_PKL/m_tiempo_pedido_normal.pkl')
+# Verificar versiones de las bibliotecas
+st.write(f"scikit-learn version:{sklearn.__version__}")
+st.write(f"joblib version:{sklearn.__version__}")
 
-# Se inicia el título inicial de la app 
+
+# Load the model
+try:
+    modelo = joblib.load('/03_PKL/m_tiempo_pedido_normal.pkl')
+except Exception as e:
+    st.error(f'Error loading model or encoders: {e}')
+
+
+# Se inicia el título inicial de la app
 st.title('Predicción de Tiempo de Pedido')
 
 # Definir inputs
@@ -16,7 +27,7 @@ def obtener_inputs():
     # Añade aquí los inputs necesarios para tu modelo
     inputs['distancia'] = st.number_input('Distancia (km)', min_value=0.0, step=0.1)
     inputs['hora_dia'] = st.number_input('Hora del día', min_value=0, max_value=23)
-    
+
     return pd.DataFrame([inputs])
 
 # Botón de predicción
