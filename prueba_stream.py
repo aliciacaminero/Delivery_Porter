d612a273db97e4f776d34d0c5239321d8ed23593
GemaVNZ <gemavnz@gmail.com>
2025-02-01 14:17:06 +0100

    Se hacen cambios.

--- prueba_stream.py
+++ prueba_stream.py
@@ -66,6 +66,27 @@ def transformar_datos(datos):
 # Título de la app
 st.title('Predicción de Tiempo de Entrega 🚚')
 
+# Estilo CSS para el slider
+NB = st.sidebar.select_slider('', options = [1,10,20,30,40,50,60,70,80,90,100], value = 1)
+
+ColorMinMax = st.markdown(''' <style> div.stSlider > div[data-baseweb = "slider"] > div[data-testid="stTickBar"] > div {
+    background: rgb(1 1 1 / 0%); } </style>''', unsafe_allow_html = True)
+
+Slider_Cursor = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div[role="slider"]{
+    background-color: rgb(14, 38, 74); box-shadow: rgb(14 38 74 / 20%) 0px 0px 0px 0.2rem;} </style>''', unsafe_allow_html = True)
+
+Slider_Number = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div > div
+                                { color: rgb(14, 38, 74); } </style>''', unsafe_allow_html = True)
+
+col = f''' <style> div.stSlider > div[data-baseweb = "slider"] > div > div {{
+    background: linear-gradient(to right, rgb(1, 183, 158) 0%, 
+                                rgb(1, 183, 158) {NB}%, 
+                                rgba(151, 166, 195, 0.25) {NB}%, 
+                                rgba(151, 166, 195, 0.25) 100%); }} </style>'''
+
+ColorSlider = st.markdown(col, unsafe_allow_html = True)
+
+
 # Contenedor principal para parámetros de entrada
 with st.container():
     col1, col2 = st.columns(2)
